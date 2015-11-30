09c3684803434bb694f876bd6769805dea24a8ee
Bojian Chen <bjchen@thoughtworks.com>
2015-11-30 21:02:37 +0800

    parking manager

---
+++ OO/ParkingManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OO
+{
+    public class ParkingManager
+    {
+        private readonly ParkingBoy stand = new ParkingBoy();
+        private readonly List<ParkingBoy> managedParkingBoys = new List<ParkingBoy>();
+        public object Park(Car car)
+        {
+            return stand.Park(car);
+        }
+
+        public void Manage(params ParkingLot[] parkingLots)
+        {
+            stand.Manage(parkingLots);
+        }
+
+        public Car Pick(object token)
+        {
+            return  stand.Pick(token);
+        }
+
+
+        public void Manage(params ParkingBoy[] parkingBoys)
+        {
+            managedParkingBoys.AddRange(parkingBoys);
+        }
+
+        public object LetParkingBoyPark(Car car)
+        {
+            foreach (var parkingBoy in managedParkingBoys)
+            {
+                var token = parkingBoy.Park(car);
+                if (token != null)
+                {
+                    return token;
+                }
+            }
+            return null;
+        }
+
+        public Car LetParkingBoyPick(object token)
+        {
+            foreach (var parkingBoy in managedParkingBoys)
+            {
+                var car = parkingBoy.Pick(token);
+                if (car != null)
+                {
+                    return car;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ OO/ParkingManagerFact.cs
@@ -0,0 +1,95 @@
+﻿using Xunit;
+
+namespace OO
+{
+    public class ParkingManagerFact
+    {
+        private readonly ParkingManager parkingManager;
+
+        public ParkingManagerFact()
+        {
+            parkingManager = new ParkingManager();
+        }
+
+        [Fact]
+        public void should_park_car_correctly()
+        {
+            var car = new Car();
+            var parkingLot = new ParkingLot();
+            parkingManager.Manage(parkingLot);
+            var token = parkingManager.Park(car);
+            Assert.Same(car, parkingLot.Pick(token));
+        }
+
+        [Fact]
+        public void should_able_to_park_cars()
+        {
+            var car = new Car();
+            parkingManager.Manage(new ParkingLot(), new ParkingLot());
+            var token = parkingManager.Park(car);
+            Assert.Same(car, parkingManager.Pick(token));
+        }
+
+        [Fact]
+        public void should_be_able_to_pick_car()
+        {
+            parkingManager.Manage(new ParkingLot());
+            var car = new Car();
+            var token = parkingManager.Park(car);
+            Assert.Same(car, parkingManager.Pick(token));
+
+        }
+
+        [Fact]
+        public void should_able_to_pick_car_in_second_parking_lots()
+        {
+            var secondParkingLot = new ParkingLot();
+            parkingManager.Manage(new ParkingLot(), secondParkingLot);
+            var car = new Car();
+            var ticket = secondParkingLot.Park(car);
+
+            Assert.Same(car, parkingManager.Pick(ticket));
+        }
+
+        [Fact]
+        public void should_able_to_let_his_parking_boy_to_park_car()
+        {
+            var parkingBoy = new ParkingBoy();
+            var parkingLot = new ParkingLot();
+            parkingBoy.Manage(parkingLot);
+            parkingManager.Manage(parkingBoy);
+            var car = new Car();
+            var token = parkingManager.LetParkingBoyPark(car);
+            Assert.Same(car, parkingBoy.Pick(token));
+        }
+
+        [Fact]
+        public void should_able_to_let_his_parking_boy_to_pick_car()
+        {
+            var parkingBoy = new ParkingBoy();
+            var parkingLot = new ParkingLot();
+            parkingBoy.Manage(parkingLot);
+            parkingManager.Manage(parkingBoy);
+            var car = new Car();
+            var token = parkingBoy.Park(car);
+            Assert.Same(car, parkingManager.LetParkingBoyPick(token));
+        }
+
+        [Fact]
+        public void should_able_to_let_his_parking_boy_who_has_availible_parking_lot_to_park_cars()
+        {
+            var parkingBoy = new ParkingBoy();
+            var smartParkingBoy = ParkingBoy.SmartEvolution();
+            parkingBoy.Manage(new ParkingLot(1));
+            smartParkingBoy.Manage(new ParkingLot(1));
+
+            parkingManager.Manage(parkingBoy, smartParkingBoy);
+
+            var car = new Car();
+            parkingBoy.Park(new Car());
+            var tokenOfParkingBoy = parkingManager.LetParkingBoyPark(car);
+            Assert.Same(car, smartParkingBoy.Pick(tokenOfParkingBoy));
+        }
+
+    }
+}
